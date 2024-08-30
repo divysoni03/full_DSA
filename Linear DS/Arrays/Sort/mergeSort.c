7f32af595df649aabedab2c3c1161bdd21188606
divy soni <chetanshudha267@gmail.com>
2024-08-30 20:34:17 +0530

    merge sort (simple version)

--- Linear DS/Arrays/Sort/mergeSort.c
+++ Linear DS/Arrays/Sort/mergeSort.c
@@ -115,6 +115,68 @@ int main() {
 }
 
 /*
+//Merge two subarrays of arr[]
+//first subarray is arr[l..m]
+//second subarray is arr[m+1..r]
+void merge(int* arr, int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid-left + 1;
+
+    int n2 = right-mid;
+
+    //Create temp arrays
+    int L[n1], R[n2];
+
+    //Copy data to temp array L[] and R[]
+    for(i=0; i<n1; i++) {
+        L[i] = arr[left+i];
+    }
+    for(j=0; j<n2; j++) {
+        R[j] = arr[mid+1+j];
+    }
+
+    //Merge the temp arrays back into arr[l..r]
+    i=0, j=0, k=left;
+    while(i < n1 && j < n2) {
+        if(L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    //Copy the remaining elements of L[], if there are any
+    while(i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while(j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int* arr, int left, int right) {
+    if(left < right) {
+        //Same as (l+r)/2, but avoids overflow for large left and right
+        int mid = (left + right)/2;
+
+        //Sort first and second halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid+1, right);
+
+        //Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+*/
+/*
 class Solution {
 public:
     // Insertion Sort for small arrays
