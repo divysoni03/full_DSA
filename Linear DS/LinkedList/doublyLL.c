6d87f4063cc9521ffb260bcf6514694b9a4bfad0
divy soni <chetanshudha267@gmail.com>
2024-09-09 22:30:28 +0530

    doubly linkedlist

---
+++ Linear DS/LinkedList/doublyLL.c
@@ -0,0 +1,233 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* prev;
+    struct Node* next;
+};
+struct Node* creatNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if(newNode == NULL) {
+        printf("Memory Allocation Failed, please try again.");
+        return NULL;
+    }
+    newNode->prev = NULL;
+    newNode->data = data;
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = creatNode(data);
+    if(*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    (*head)->prev = newNode;
+    newNode->next = *head;
+
+    *head = newNode;
+}
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = creatNode(data);
+    struct Node* curr = *head;
+    if(*head == NULL) {
+        *head = newNode;
+        return; 
+    }
+    while(curr->next != NULL) {
+        curr = curr->next;
+    }
+
+    curr->next = newNode;
+    newNode->prev = curr;
+}
+void insertAtPosition(struct Node** head, int data, int position) {
+    if(position < 1) {
+        printf("Invalid Position, please Try again.");
+        return;
+    }
+    struct Node* newNode = creatNode(data);
+    if(position == 1) {
+        newNode->next = *head;
+        *head = newNode;
+        return;
+    }
+
+    struct Node* curr = *head;
+    for(int i=1; i<position-1; i++) {
+        if(curr == NULL) {
+            printf("Position is out of bound, please enter valid position.");
+            return;
+        }
+        curr = curr->next;
+    }
+    newNode->next = curr->next;
+    (curr->next)->prev = newNode;
+    newNode->prev = curr;
+    curr->next = newNode;
+}
+void deleteAtBeginning(struct Node** head) {
+    if(*head == NULL) return;
+    if((*head)->next == NULL) {
+        struct Node* temp = *head;
+        *head = NULL;
+        free(temp);
+        return;
+    }
+    struct Node* temp = *head;
+    *head = (*head)->next;
+    (*head)->prev = NULL;
+    free(temp);
+}
+
+void print(struct Node* head) {
+    printf("\nLinkedList : head ");
+    while(head != NULL) {
+        printf("<- %d -> ", head->data);
+        head = head->next;
+    }
+    printf(" NULL");
+}
+int main() {
+    struct Node* head = NULL;
+    
+    int choice = 0,data = 0, position = 0;
+    do {
+        system("cls");
+        print(head);
+        printf("\n1.Insert At Beginning");
+        printf("\n2.Insert At End");
+        printf("\n3.Insert At Postion");
+        printf("\n4.Delete At Beginning");
+        // printf("\n");
+        // printf("\n");
+        // printf("\n");
+        printf("\nEnter Your Choice :");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("\nEnter Data :");
+                scanf("%d", &data);
+                insertAtBeginning(&head, data);
+                break;
+            case 2:
+                printf("\nEnter Data :");
+                scanf("%d", &data);
+                insertAtEnd(&head, data);
+                break;
+            case 3:
+                printf("\nEnter Data :");
+                scanf("%d", &data);
+                printf("\nEnter Position :");
+                scanf("%d", &position);
+                insertAtPosition(&head, data, position);
+                break;
+            case 4:
+                deleteAtBeginning(&head);
+                break;
+            case 0:
+                printf("Exiting the program.");
+                free(head);
+                break;
+            default : 
+                printf("Invalid Input, please Try again.");
+                break;
+        }
+    }while(choice != 0);
+    return 0;
+}
+
+
+
+// #include<stdio.h>
+// #include<stdlib.h>
+
+// typedef struct Node* node;
+// struct Node {
+//     int data;
+//     node prev;
+//     node next;
+// };
+
+// node createNode(int data) {
+//     node newNode = (node)malloc(sizeof(struct Node));
+//     if (newNode == NULL) {
+//         printf("\nMemory Allocation Failed!");
+//         exit(1);
+//     }
+//     newNode->prev = NULL;
+//     newNode->data = data;
+//     newNode->next = NULL;
+
+//     return newNode;
+// }
+
+// // Insert At Front
+// node insertAtFront(node head, int data) {
+//     node newNode = createNode(data);
+//     newNode->next = head;
+
+//     if(head != NULL) {
+//         head->prev = newNode;
+//     }
+//     return newNode;
+// }
+
+// node insertAtEnd(node head, int data) {
+//     node newNode = createNode(data);
+
+//     if(head == NULL) {
+//         return newNode;
+//     }
+
+//     node curr = head;
+//     while(curr->next != NULL) {
+//         curr = curr->next;
+//     }
+
+//     curr->next = newNode;
+//     newNode->prev = curr;
+
+//     return head;
+// }
+
+// void printForward(node head) {
+//     printf("\nJust List > \tNULL <-> ");
+//     while(head != NULL) {
+//         printf("%d <-> ", head->data);
+//         head = head->next;
+//     }
+//     printf("NULL\n\n");
+// }
+
+// int main() {
+//     node list = NULL;
+
+//     // list = insertAtFront(list, 11);
+//     // list = insertAtFront(list, 12);
+//     // list = insertAtFront(list, 13);
+//     // list = insertAtFront(list, 14);
+//     // list = insertAtFront(list, 15);
+//     // list = insertAtFront(list, 16);
+//     // list = insertAtFront(list, 17);
+//     // list = insertAtFront(list, 18);
+//     // list = insertAtFront(list, 19);
+//     // list = insertAtFront(list, 20);
+
+//     list = insertAtEnd(list, 11);
+//     list = insertAtEnd(list, 12);
+//     list = insertAtEnd(list, 13);
+//     list = insertAtEnd(list, 14);
+//     list = insertAtEnd(list, 15);
+//     list = insertAtEnd(list, 16);
+//     list = insertAtEnd(list, 17);
+//     list = insertAtEnd(list, 18);
+//     list = insertAtEnd(list, 19);
+//     list = insertAtEnd(list, 20);
+
+//     printForward(list);
+// }
