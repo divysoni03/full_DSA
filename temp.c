6974cc04114fe573ec274da5fad4db9681fc975b
divy soni <chetanshudha267@gmail.com>
2024-09-20 00:54:57 +0530

    simple que

--- temp.c
+++ temp.c
@@ -1,53 +1,59 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+ 
+#include <time.h>
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
 
-int partition(int *arr, int start, int end) {
-    int pivot = arr[end], i = start -1;
-    for(int j=start; j<end-1; j++) {
-        if(arr[j] < pivot) {
-            i++;
-            swap(&arr[i], &arr[j]);
+void bubbleSort(int array[], int size) {
+    int i, j, temp;
+
+    for (i = 0; i < size - 1; i++) {
+        for (j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
         }
     }
-    swap(&arr[i+1], &arr[end]);
-    return i+1;
-}
-void quickSort(int *arr, int start, int end) {
-    if(start < end) {
-        int pivot = partition(arr, start, end);
-        quickSort(arr, start, pivot-1);
-        quickSort(arr, pivot+1, end);
-    }
 }
 
-void testFunction() {
-    int size = 10000;
-    int *arr = (int *)calloc(size, sizeof(int));
 
-    for(int i=0; i<size; i++) {
-        arr[i] = rand() % 1000;
+void printArray(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
     }
-
-    printf("\nSorting Started.");
-    clock_t start = clock();
-    quickSort(arr, 0, size-1);
-    clock_t end = clock();
-    printf("\nSorting Done.");
-
-    double time = ((double)(end-start)) / CLOCKS_PER_SEC;
-    printf("\nQuick Sort Took %.8f sec to sort %d size of array.", time, size); 
-
+    printf("\n");
 }
 
 int main() {
-    srand(time(NULL)); //makes random element based on local time
-    testFunction();
+   
+      
+    time_t now = time(NULL);
+    struct tm *currentDateTime = localtime(&now);
+
+    
+    printf("Enrollment Number:230130107107 \nName: Pattani divy \n");    printf("Current Date: %d-%d-%d\n", currentDateTime->tm_year + 1900, currentDateTime->tm_mon + 1, currentDateTime->tm_mday);
+    printf("Current Time: %d:%d:%d\n", currentDateTime->tm_hour, currentDateTime->tm_min, currentDateTime->tm_sec);
+
+    int arraySize;
+    printf("Enter the size of the array: ");
+    scanf("%d", &arraySize);
+
+    int numbers[arraySize];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < arraySize; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    printf("Original array: ");
+    printArray(numbers, arraySize);
+
+    bubbleSort(numbers, arraySize);
+
+    printf("Sorted array: ");
+    printArray(numbers, arraySize);
+    
+     
     return 0;
+   
 }
